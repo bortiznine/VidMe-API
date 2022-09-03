e994a1e464999600141214e78735382f572935b3
Brian O <bortiznine@gmail.com>
2022-09-03 10:17:53 -0500

    Added feature branch for Unit test coverage

---
+++ src/test/java/com/vidme/demo/controller/UserControllerTest.java
@@ -0,0 +1,12 @@
+package com.vidme.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    public void
+
+}
