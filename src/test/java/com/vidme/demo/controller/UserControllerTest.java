f780e8448a02b7353a7cb96895d81f7830f32d65
pengu <bortiznine@gmail.com>
2022-09-03 10:53:15 -0500

    setting up tests for userService, need to find source of failed state as dbConfig used postgres

--- src/test/java/com/vidme/demo/controller/UserControllerTest.java
+++ src/test/java/com/vidme/demo/controller/UserControllerTest.java
@@ -2,12 +2,16 @@ package com.vidme.demo.controller;
 
 import com.vidme.demo.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserControllerTest {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 @Test
     public void setUserServiceTest(){
         UserService ust= new UserService();
---
+++ src/test/java/com/vidme/demo/service/VidMeServiceTest.java
@@ -0,0 +1,13 @@
+package com.vidme.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VidMeServiceTest {
+
+    @Test
+    public void setUserServiceTest(){
+
+    }
+}
